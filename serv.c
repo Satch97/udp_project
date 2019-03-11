771172724311ad55e48643d301b9fa2aaf2c0911
Satchin Campbell <satchin.campbell@gmail.com>
2019-03-11 13:08:44 -0700

    Read port number from command line

--- serv.c
+++ serv.c
@@ -1,3 +1,16 @@
-int main() {
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+#include <errno.h>
+int main(int argc, char **argv) {
+	if(argc != 2) {
+		printf("Usage Error : server.exe <server port>");
+		exit(EXIT_FAILURE);
+	}
+
+	long int rawPort = strtol(argv[2], NULL, 10);
+	uint16_t port = (uint16_t) rawPort;
+
 	return 0;
 }
