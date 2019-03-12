5ab9cd62f256d426acc372451baf4bd66a5886dc
Satchin Campbell <satchin.campbell@gmail.com>
2019-03-11 22:17:15 -0700

    Use correct arg number

--- serv.c
+++ serv.c
@@ -9,7 +9,7 @@ int main(int argc, char **argv) {
 		exit(EXIT_FAILURE);
 	}
 
-	long int rawPort = strtol(argv[2], NULL, 10);
+	long int rawPort = strtol(argv[1], NULL, 10);
 	uint16_t port = (uint16_t) rawPort;
 
 	return 0;
