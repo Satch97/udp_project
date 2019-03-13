fdd677b9663d836655e2ae981f37baabd136888e
Satchin Campbell <satchin.campbell@gmail.com>
2019-03-13 10:06:02 -0700

    Read CLAs

--- clnt.c
+++ clnt.c
@@ -1,3 +1,10 @@
-int main() {
-	return 0;
+#include <errno.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+	if(argc != 3) {
+		printf("Usage Error : client.exe <server IP> <server port>");
+		exit(EXIT_FAILURE);
+	}
 }
