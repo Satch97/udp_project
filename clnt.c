b58b2bbc6abeed43b7d20cfebff94ebd278eaff7
Satchin Campbell <satchin.campbell@gmail.com>
2019-03-14 11:11:58 -0700

    Add Network Headers

--- clnt.c
+++ clnt.c
@@ -1,3 +1,9 @@
+
+#include <sys/socket.h>       /*  socket definitions        */
+#include <sys/types.h>        /*  socket types              */
+#include <arpa/inet.h>        /*  inet (3) funtions         */
+#include <unistd.h>           /*  misc. UNIX functions      */
+
 #include <errno.h>
 #include <stdlib.h>
 #include <stdio.h>
