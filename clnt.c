bae58e4c69cd6c7c264816b326f4b75537fd8cfa
Satchin Campbell <satchin.campbell@gmail.com>
2019-03-14 10:33:49 -0700

    Validate and capture port

--- clnt.c
+++ clnt.c
@@ -7,4 +7,12 @@ int main(int argc, char **argv) {
 		printf("Usage Error : client.exe <server IP> <server port>");
 		exit(EXIT_FAILURE);
 	}
+
+	long int rawPort = strtol(argv[2], NULL, 10);
+	uint16_t port = (uint16_t) rawPort;
+
+	if(port != rawPort) {
+		printf("Invalid port"); // does not fit in range
+		exit(EXIT_FAILURE);
+	}
 }
